722983e258c32d6cbd9ca3a3f775bad54056d2e2
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2018-07-19 13:04:33 +0200

    qt: mlbasemodel: use delegating constructor

--- modules/gui/qt/components/mediacenter/mlbasemodel.cpp
+++ modules/gui/qt/components/mediacenter/mlbasemodel.cpp
@@ -1,17 +1,6 @@
 #include <cassert>
 #include "mlbasemodel.hpp"
 
-MLBaseModel::MLBaseModel(vlc_medialibrary_t* ml, QObject *parent)
-    : QAbstractListModel(parent)
-    , m_ml(ml)
-{
-    assert(ml != nullptr);
-    memset(&m_query_param, 0, sizeof(vlc_ml_query_params_t));
-    m_query_param.b_desc = false;
-    m_query_param.i_nbResults = 20; //FIXME: value for test
-    m_query_param.i_sort = VLC_ML_SORTING_DEFAULT;
-}
-
 MLBaseModel::MLBaseModel(vlc_medialibrary_t* ml, vlc_ml_parent_type parent_type, uint64_t parent_id, QObject *parent)
     : QAbstractListModel(parent)
     , m_parent_type (parent_type)
@@ -23,6 +12,11 @@ MLBaseModel::MLBaseModel(vlc_medialibrary_t* ml, vlc_ml_parent_type parent_type,
     m_query_param.b_desc = false;
     m_query_param.i_nbResults = 20; //FIXME: value for test
     m_query_param.i_sort = VLC_ML_SORTING_DEFAULT;
+}
+
+MLBaseModel::MLBaseModel(vlc_medialibrary_t* ml, QObject *parent)
+    : MLBaseModel(ml, static_cast<vlc_ml_parent_type>( -1 ), 0, parent)
+{
 }
 
 MLBaseModel::~MLBaseModel()
